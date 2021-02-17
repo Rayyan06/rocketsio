42cb2496884848fa2d387103c1dd6c9b404b2ffc
Rayyan Khan <58400533+Rayyan06@users.noreply.github.com>
2021-02-17 07:24:17 -0500

    initial commit

---
+++ src/server/collisions.js
@@ -0,0 +1,26 @@
+const Constants = require("../shared/constants");
+
+// Return an array on bullets to be destroyed.
+function applyCollisions(players, bullets) {
+  const destroyedBullets = [];
+  for (let i = 0; i < bullets.length; i++) {
+    // Look for a player who did not break the bullet to collide with the bullet.
+    // When we find one, break
+    for (let j = 0; j < players.length; j++) {
+      const bullet = bullets[i];
+      const player = players[j];
+      if (
+        bullet.parentID !== player.id &&
+        player.distanceTo(bullet) <=
+          Constants.PLAYER_RADIUS + Constants.BULLET_RADIUS
+      ) {
+        destroyedBullets.push(bullet);
+        player.takeBulletDamage();
+        break;
+      }
+    }
+  }
+  return destroyedBullets;
+}
+
+module.exports = applyCollisions;
