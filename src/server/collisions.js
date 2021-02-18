1ae509889149b38dde1e3d5a17d165f08ac8fc29
Rayyan Khan <58400533+Rayyan06@users.noreply.github.com>
2021-02-18 15:49:20 -0500

    Fixed bug

--- src/server/collisions.js
+++ src/server/collisions.js
@@ -34,7 +34,7 @@ function applyPlayerCollisions(players) {
         player.id !== otherPlayer.id &&
         player.distanceTo(otherPlayer) <= Constants.PLAYER_RADIUS * 2
       ) {
-        collidedPlayers.push({ player: player, otherPlayer: otherPlayer);
+        collidedPlayers.push({ player: player, otherPlayer: otherPlayer });
         player.isColliding = true;
         break;
       } else {
